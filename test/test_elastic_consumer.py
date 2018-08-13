c878f603a6d98ab1a160b624849163280460feee
Kordishal <jonaswaeber@gmail.com>
2018-08-13 12:22:57 +0200

    v0.2.3 Added test for elastic consumer.

--- test/test_elastic_consumer.py
+++ test/test_elastic_consumer.py
@@ -2,7 +2,6 @@ import logging
 import os
 import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from kafka_event_hub.consumers import ElasticConsumer
@@ -11,13 +10,16 @@ from kafka_event_hub.consumers import ElasticConsumer
 class TestElasticConsumer(object):
 
     def setup_class(self):
-        self.consumer = ElasticConsumer('configs/elastic_consumer/elastic_test.yml')
+        self.consumer = ElasticConsumer('configs/elastic_consumer/elastic_consumer.yml')
 
     def test_consume(self):
         self.consumer.consume()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     consumer = TestElasticConsumer()
+    logging.debug('Setup class')
     consumer.setup_class()
+    logging.debug('Consume message')
     consumer.test_consume()
