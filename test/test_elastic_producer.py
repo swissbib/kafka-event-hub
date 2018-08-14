616a68d27e2b7acf96dedf3c38815c3b433023a9
Kordishal <jonaswaeber@gmail.com>
2018-08-14 17:23:00 +0200

    fix(producers-elastic): Refactored test for elastic producer

--- test/test_elastic_producer.py
+++ test/test_elastic_producer.py
@@ -1,25 +1,16 @@
-import logging
-import os
-import sys
-
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from kafka_event_hub.producers import ElasticProducer
 
 
 class TestElasticProducer(object):
 
     def setup_class(self):
+        import logging
+        import os
+        import sys
+
+        sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        from kafka_event_hub.producers import ElasticProducer
+
         self.producer = ElasticProducer('configs/elastic/elastic_producer.yml')
 
     def test_produce(self):
         self.producer.process()
-
-
-if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    consumer = TestElasticProducer()
-    logging.debug('Setup class')
-    consumer.setup_class()
-    logging.debug('Produce message')
-    consumer.test_produce()
