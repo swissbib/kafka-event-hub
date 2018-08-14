34fb8042e91ea8c83702473af90cb3fb6e0a7083
Kordishal <jonaswaeber@gmail.com>
2018-08-14 17:31:29 +0200

    feat(test): Improved tests to be run with pytest.

--- test/test_elastic_consumer.py
+++ test/test_elastic_consumer.py
@@ -3,13 +3,14 @@ import os
 import sys
 
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from kafka_event_hub.consumers import ElasticConsumer
+
 
 
 class TestElasticConsumer(object):
 
     def setup_class(self):
+        sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        from kafka_event_hub.consumers import ElasticConsumer
         self.consumer = ElasticConsumer('configs/elastic/elastic.yml')
 
     def test_consume(self):
