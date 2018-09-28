13de04987c401014ad7ae9a9525bfb6c0e57035f
Jonas Waeber <jonaswaeber@gmail.com>
2018-09-28 19:55:39 +0200

    test(oai): Add some tests for oai.

--- kafka_event_hub/producers/oai/oai_producer.py
+++ kafka_event_hub/producers/oai/oai_producer.py
@@ -31,63 +31,46 @@ class OAIProducer(AbstractBaseProducer):
         :param configuration: The path to the configuration file.
         """
         super().__init__(configuration, OAIConfig)
-        self.recordBodyRegEx = re.compile(self.configuration['Processing']['recordBodyRegEx'], re.UNICODE | re.DOTALL | re.IGNORECASE)
+        self._record_body_regex = re.compile(self.configuration['Processing']['recordBodyRegEx'], re.UNICODE | re.DOTALL | re.IGNORECASE)
         self.configuration.update_start_time()
+
+    @property
+    def record_body_regex(self):
+        return self._record_body_regex
 
     def process(self):
         try:
 
-            sickle = Sickle(self.configuration['OAI']['url'])
-            #print(sickle.endpoint)
+            sickle = Sickle(self.configuration['oai']['base']['url'])
             dic = {}
             if not self.configuration['OAI']['metadataPrefix'] is None:
-                dic['metadataPrefix'] =  self.configuration['OAI']['metadataPrefix']
+                dic['metadataPrefix'] = self.configuration['OAI']['metadataPrefix']
             if not self.configuration['OAI']['setSpec'] is None:
                 dic['set'] = self.configuration['OAI']['setSpec']
             if not self.configuration['OAI']['timestampUTC'] is None:
                 dic['from'] = transform_from_until(self.configuration['OAI']['timestampUTC'],
-                                                             self.configuration['OAI']['granularity'])
+                                                   self.configuration['OAI']['granularity'])
             if not self.configuration['OAI']['until'] is None:
                 dic['until'] = transform_from_until(self.configuration['OAI']['until'],
-                                                              self.configuration['OAI']['granularity'])
+                                                    self.configuration['OAI']['granularity'])
 
-            #for k, v in dic.items():
-            #    print(k, v)
-
-            recordsIt = sickle.ListRecords(
-                **dic
+            records_iter = sickle.ListRecords(
+                **self.configuration['oai']
             )
 
-
-            for record in recordsIt:
-                #print(record.header.identifier)
-                #print(record.header.datestamp)
-                #no at the moment we store all the data
-                #if record.header.deleted:
-                    #at the moment in time I don't want to use deleted items in Kafka
-                #    continue
-                #we want the raw data coming directly from the source
-                #this is going to be stored in Kafka and later used for the persistent storage
-                #sBody = self.recordBodyRegEx.search(record.raw)
-                #if sBody:
-                #    body = sBody.group(1)
-                    #todo
-                    #key should not contain the ID of the OAI identifier (sysID of library system
-                    #otherwise partitions for single networks won't be stable
+            for record in records_iter:
                 self._produce_kafka_message(value=record.raw,
                                             key=record.header.identifier,
                                             eventTime=record.header.datestamp)
-                #else:
-                #    raise Exception("we havent't found the body which should not be the case")
 
         except BadArgument as ba:
-            print(ba)
+            self._logger.exception(ba)
         except OAIError as oaiError:
-            print(oaiError)
+            self._logger.exception(oaiError)
         except Exception as baseException:
-            print(baseException)
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
+            self._logger.exception(baseException)
+        else:
+            self.update_configuration()
 
     def update_configuration(self):
         self.configuration.update_stop_time()
