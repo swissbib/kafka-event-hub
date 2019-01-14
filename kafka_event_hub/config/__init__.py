10cc5f1c17f1fa16fa555d94e0014f01d8274874
Jonas Waeber <jonaswaeber@gmail.com>
2019-01-14 17:07:55 +0100

    feat(ElasticProducerConfig): Adds ElasticProducerConfig

--- kafka_event_hub/config/__init__.py
+++ kafka_event_hub/config/__init__.py
@@ -1,1 +1,1 @@
-from kafka_event_hub.config.event_hub_config import BaseConfig, OAIConfig, ProducerConfig, LineProducerConfig, ConsumerConfig
+from kafka_event_hub.config.event_hub_config import BaseConfig, OAIConfig, ProducerConfig, LineProducerConfig, ConsumerConfig, ElasticProducerConfig
