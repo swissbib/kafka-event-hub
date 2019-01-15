a9033e43ef453ea21907bf91657562fbbcfb8bb9
Jonas Waeber <jonaswaeber@gmail.com>
2019-01-15 10:32:33 +0100

    test(Tests): Fix and update tests and configs.

--- test/test_elastic_consumers.py
+++ test/test_elastic_consumers.py
@@ -1,21 +1,35 @@
 import sys
 import os
 import logging
+import pytest
 
-logging.basicConfig(filename='logs/test-elastic-consumer.log', filemode='w', level=logging.DEBUG)
+logging.basicConfig(filename='logs/test-elastic-consumer.log', filemode='w', level=logging.ERROR)
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from kafka_event_hub.consumers import SimpleElasticConsumer
 from kafka_event_hub.producers import LineProducer
 
+from kafka import KafkaAdminClient
+
 class TestSimpleElasticConsumer(object):
 
     def setup_class(self):
+        self.admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
         self.consumer = SimpleElasticConsumer('configs/elastic/elastic-consumer-simple-test.yml')
         self.producer = LineProducer("configs/elastic/json-lines-producer.yml")
 
+    def teardown_class(self):
+        self.admin.delete_topics(['json'])
+        self.consumer.close()
+
+
+    # @pytest.mark.skip()
     def test_consume(self):
         self.producer.process()
-        for _ in range(2):
-            self.consumer.consume(10)
+        
+        count = 0
+        while self.consumer.consume():
+            count += 1
+            if count == 4:
+                break
--- test/test_lines_producer.py
+++ test/test_lines_producer.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import logging
+import pytest
 
 logging.basicConfig(filename='logs/line_producer.log', filemode='w', level=logging.ERROR)
 
@@ -12,10 +13,7 @@ from kafka import KafkaAdminClient
 class TestLineProducer(object):
 
     def setup_class(self):
-
         self.admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
-        self.admin.delete_topics(['test-lines-gz-v3'])
-        self.admin.delete_topics(['test-lines-v3'])
         self.producer = LineProducer('configs/lines/producer.yml')
         self.producer_gz = LineProducer('configs/lines/producer_gz.yml')
         self.consumer = SimpleConsumer('configs/lines/consumer.yml')
@@ -27,70 +25,72 @@ class TestLineProducer(object):
 
         self.admin.delete_topics(['test-lines-gz-v3'])
         self.admin.delete_topics(['test-lines-v3'])
+        self.admin.close()
 
-
+    #@pytest.mark.skip()
     def test_produce(self):
         self.producer.process()
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer.consume()
         assert key == '0'
         assert message == "This is a line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer.consume()
         assert key == '1'
         assert message == "and another line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer.consume()
         assert key == '2'
         assert message == "a third line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer.consume()
         assert key == '3'
         assert message == "a forth line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer.consume()
         assert key == '4'
         assert message == "a lot of lines now"
 
+    #@pytest.mark.skip("Currently way too slow")
     def test_produce_gz(self):                
         self.producer_gz.process()
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer_gz.consume()
         assert key == '0'
         assert message == "This is a line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer_gz.consume()
         assert key == '1'
         assert message == "and another line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer_gz.consume()
         assert key == '2'
         assert message == "a third line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer_gz.consume()
         assert key == '3'
         assert message == "a forth line"
         key = None
         message = None
-        while key == None and message == None:
+        while key is None and message is None:
             key, message = self.consumer_gz.consume()
         assert key == '4'
         assert message == "a lot of lines now"
