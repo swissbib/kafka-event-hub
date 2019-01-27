bef50d93a6c95179945907d5ee148c436b249ea4
guenter hipler <guenter.hipler@unibas.ch>
2019-01-27 20:05:18 +0100

    changed admin scripts to delete and create topics

--- admin/delete_content_collector_topics_part1.sh
+++ admin/delete_content_collector_topics_part1.sh
@@ -3,7 +3,7 @@
 KAFKA_BASE=/usr/local/swissbib/kafka
 KAFKA_BASE1=/usr/local/swissbib/kafka1
 ZK_HOST=sb-uka1.swissbib.unibas.ch:4181,sb-uka2.swissbib.unibas.ch:4181,sb-uwf3.swissbib.unibas.ch:4181/kafka
-ZK_HOST_KAFKA=localhost:4181/kafka
+ZK_HOST_KAFKA=localhost:4181
 
 #for topic in IDSABN IDSALEX ALEXREPO IDSBGR BORIS ECOD EDOC EPERIODICA ETHRESEARCH HEMU IDSBB IDSLU IDSSG KBTG LIBIB NATIONALBIBLIOTHEK POSTERS IDSSBT SERVAL SGBN VAUD ZORA
 for topic in IDSABN ALEX ALEXREPO IDSBGR BORIS ECOD EDOC EPERIODICA ETHRESEARCH HEMU IDSBB
@@ -12,8 +12,8 @@ for topic in IDSABN ALEX ALEXREPO IDSBGR BORIS ECOD EDOC EPERIODICA ETHRESEARCH 
 do
     echo "delete topic $topic"
 
-    ssh swissbib@sb-uka1.swissbib.unibas.ch "source /home/swissbib/.bash_profile; $KAFKA_BASE/bin/kafka-topics.sh --zookeeper $ZK_HOST --delete --topic $topic"
-    #$KAFKA_BASE1/bin/kafka-topics.sh --zookeeper $ZK_HOST_KAFKA --delete --topic $topic
+    #ssh swissbib@sb-uka1.swissbib.unibas.ch "source /home/swissbib/.bash_profile; $KAFKA_BASE/bin/kafka-topics.sh --zookeeper $ZK_HOST --delete --topic $topic"
+    $KAFKA_BASE1/bin/kafka-topics.sh --zookeeper $ZK_HOST_KAFKA --delete --topic $topic
 
 
 done
--- admin/delete_content_collector_topics_part2.sh
+++ admin/delete_content_collector_topics_part2.sh
@@ -3,7 +3,7 @@
 KAFKA_BASE=/usr/local/swissbib/kafka
 KAFKA_BASE1=/usr/local/swissbib/kafka1
 ZK_HOST=sb-uka1.swissbib.unibas.ch:4181,sb-uka2.swissbib.unibas.ch:4181,sb-uwf3.swissbib.unibas.ch:4181/kafka
-ZK_HOST_KAFKA=localhost:4181/kafka
+ZK_HOST_KAFKA=localhost:4181
 
 for topic in IDSLU IDSSG KBTG LIBIB NATIONALBIBLIOTHEK POSTERS IDSSBT SERVAL SGBN VAUD ZORA
 
@@ -11,7 +11,7 @@ for topic in IDSLU IDSSG KBTG LIBIB NATIONALBIBLIOTHEK POSTERS IDSSBT SERVAL SGB
 do
     echo "delete topic $topic"
 
-    ssh swissbib@sb-uka1.swissbib.unibas.ch "source /home/swissbib/.bash_profile; $KAFKA_BASE/bin/kafka-topics.sh --zookeeper $ZK_HOST --delete --topic $topic"
-    #$KAFKA_BASE1/bin/kafka-topics.sh --zookeeper $ZK_HOST_KAFKA --delete --topic $topic
+    #ssh swissbib@sb-uka1.swissbib.unibas.ch "source /home/swissbib/.bash_profile; $KAFKA_BASE/bin/kafka-topics.sh --zookeeper $ZK_HOST --delete --topic $topic"
+    $KAFKA_BASE1/bin/kafka-topics.sh --zookeeper $ZK_HOST_KAFKA --delete --topic $topic
 
 done
