14120da7156414660d605826d09f0ac9f955519b
Jonas Waeber <jonaswaeber@gmail.com>
2019-02-25 15:28:11 +0100

    feat(producer): Add SortedNTriplesCollectionProducer
    This producer takes a sorted ntriples file and publishes
    one message per subject. Does not work if not sorted...

--- test/test_elastic_producer.py
+++ test/test_elastic_producer.py
@@ -3,7 +3,7 @@ import os
 import logging
 import pytest
 
-logging.basicConfig(filename='logs/elastic-producer-tests.log', filemode='w', level=logging.DEBUG)
+# logging.basicConfig(filename='logs/elastic-producer-tests.log', filemode='w', level=logging.DEBUG)
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from kafka_event_hub.producers import ElasticProducer
